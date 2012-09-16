94dab81952f6b44d8841fea052a286a6f6b65625
unknown <Administrator@celab1-18.comp1-domain.spit.ac.in>
2012-09-16 15:09:31 +0530

    final of day 2

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,9 @@
 class Company < ActiveRecord::Base
+  validate :company_symbol_3or4
+
+  def company_symbol_3or4
+    if symbol.length < 3 || symbol.length >4
+      errors.add :symbol, 'Should be of length 3 or 4'
+    end
+  end
 end
