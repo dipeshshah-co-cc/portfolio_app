d31e064b9d27dfab899d0a3e9ae5e6160a4f4143
unknown <Administrator@celab1-18.comp1-domain.spit.ac.in>
2012-09-16 14:50:50 +0530

    with own validations on investment

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,4 +1,18 @@
 class Investment < ActiveRecord::Base
   belongs_to :company
   belongs_to :portfolio
+
+  validate :quantity_should_be_positive
+    def quantity_should_be_positive
+      if qty < 0
+        errors.add :qty, 'Should be +ve'
+      end
+    end
+
+  validate :cost_should_be_positive
+  def cost_should_be_positive
+    if cost < 0
+      errors.add :cost, 'Should be +ve'
+    end
+  end
 end
