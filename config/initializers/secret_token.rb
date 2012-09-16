9ed966003d1abba09ecd71ed5abb09d215b4a99b
unknown <Administrator@celab1-18.comp1-domain.spit.ac.in>
2012-09-16 14:32:17 +0530

    new portfolio_app with relatios and dropdown

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioApp::Application.config.secret_token = '0c7bc07e041b746d1e0c1a195af29eed7b386d3872d11dbcc3014e1700f227be11ee960a75e880fc84901a2d101397bca5e04b953b2d9932eefae901fc7294d4'
